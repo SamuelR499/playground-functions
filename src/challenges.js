dc33c1ba5cd53916a0d75384ef6f4120d2134a6c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-03-11 17:23:21 -0300

    resolução requisito 4, 5 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,12 @@
 // Desafio 1
 function compareTrue(a, b) {
   // seu código aqui
-    if(a === true && b === true){
-      return true;
-    }else{
-      return false;
-    }
+  if ( a===true && b === true){
+    return true;
+  } else {
+    return false;
   }
-  // console.log(compareTrue(true, true));
-
+}
 
 // Desafio 2
 function calcArea(base, height) {
@@ -16,36 +14,51 @@ function calcArea(base, height) {
   let trianguloArea = (base * height) / 2;
   return trianguloArea;
 }
-  // console.log(calcArea(51, 1));
+// console.log(calcArea(51, 1));
 
 
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
   let arraySplitString = string.split("\ ");
-    return arraySplitString;
+  return arraySplitString;
 };
-  // console.log(splitSentence("go Trybe"));
-  // console.log(splitSentence("vamo que vamo"));
-  // console.log(splitSentence("foguete"));
+// console.log(splitSentence("go Trybe"));
+// console.log(splitSentence("vamo que vamo"));
+// console.log(splitSentence("foguete"));
 
 
 // Desafio 4
 function concatName(stringArray) {
   // seu código aqui
-
+  let lastItem = stringArray[stringArray.length - 1]
+  let firstItem = stringArray[0];
+  let concat = lastItem + ", " + firstItem;
+  return concat;
 }
-
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let vitoria = wins *3;
+  let empate = ties;
+  let soma = vitoria + empate;
+  return  soma;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numero) {
   // seu código aqui
+  
+  let maior = Math.max.apply(null, numero);
+  let contador = 0;
+  for(i in numero){
+    if(maior === numero[i]){
+      contador += 1 
+    }
+  }
+    return contador;
 }
-
+ 
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
